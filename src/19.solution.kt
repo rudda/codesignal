ae5fd067754206542113cc488dab43a6b11a793b
rudda <beltrao.rudah@gmail.com>
2024-07-22 23:58:32 -0400

    solução melhorada

--- src/19.solution.kt
+++ src/19.solution.kt
@@ -37,3 +37,32 @@ class Logger() {
  * var obj = Logger()
  * var param_1 = obj.shouldPrintMessage(timestamp,message)
  */
+
+
+ /**
+solução melhorada
+
+class Logger() {
+    private val mapper = hashMapOf<String, Int>()
+
+    fun shouldPrintMessage(timestamp: Int, message: String): Boolean {
+      // case 1: [] -> return null
+      // edge case 1: can I have a negative timestamp ? 
+      // edge case 2: can I have a empty or null message string ?
+      if(timestamp < 0 || message.isEmpty()) {
+        return false
+      }
+       
+      // store the values and uptade
+      // hasmap where the key is the message and the value is the next allowed timestamp
+      val nextAllowedTimestamp = mapper.get(message)
+      return if(nextAllowedTimestamp ==  null || timestamp >= nextAllowedTimestamp) {
+            mapper.put(message, timestamp.plus(10))
+            true
+        } else  {
+            false
+        }
+    }
+
+}
+ */
